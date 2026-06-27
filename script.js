489dbd7b4dcaab4487cd39beea7b75081829817d
jesseperez00181978-tech <jesseperez00181978@gmail.com>
2026-06-27 09:38:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
-    alert("🎃 Welcome to HorrorStack!");
+    const title = document.querySelector("#movieTitle").value;
+    const year = document.querySelector("#movieYear").value;
+    const genre = document.querySelector("#movieGenre").value;
+    const rating = document.querySelector("#movieRating").value;
+
+    console.log(title);
+    console.log(year);
+    console.log(genre);
+    console.log(rating);
 });
