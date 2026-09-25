2b3175743e1a0c8e50235f3adb9c2e63c9e9e351
jesseperez00181978-tech <jesseperez00181978@gmail.com>
2026-09-24 19:43:40 -0600

    Add files via upload

--- app/src/main/java/com/horrorstack/tv/MainActivity.java
+++ app/src/main/java/com/horrorstack/tv/MainActivity.java
@@ -1,1 +1,39 @@
+package com.horrorstack.tv;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setMediaPlaybackRequiresUserGesture(false);
+        s.setAllowFileAccess(true);
+        s.setAllowContentAccess(true);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.addJavascriptInterface(new NativeBridge(), "HorrorStackAndroid");
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    public final class NativeBridge {
+        @JavascriptInterface
+        public void play(String url, String title) {
+            runOnUiThread(() -> {
+                Intent intent = new Intent(MainActivity.this, PlayerActivity.class);
+                intent.putExtra("url", url);
+                intent.putExtra("title", title);
+                startActivity(intent);
+            });
+        }
+    }
+}
