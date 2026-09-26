784b6141803a27b6babfd008e64830089fd9950c
jesseperez00181978-tech <jesseperez00181978@gmail.com>
2026-09-26 13:57:14 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,24 @@
+plugins {
+    id("com.android.application")
+}
 
+android {
+    namespace = "com.horrorstack.tv"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.horrorstack.tv"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.1")
+
+    implementation("androidx.media3:media3-exoplayer:1.11.1")
+    implementation("androidx.media3:media3-exoplayer-hls:1.11.1")
+    implementation("androidx.media3:media3-ui:1.11.1")
+}
